93813d224daafb9e5e294313fc06c00aba6fdae1
J M Adis <jessicaadis7@gmail.com>
2023-12-04 15:52:55 +0000

    have generated a random list of letters

--- wordsearch/src/main.ts
+++ wordsearch/src/main.ts
@@ -1,4 +1,25 @@
 import "../styles/main.scss"
 
-console.log("Hello World")
+// DEFININING WORD LIST FOR WORDSEARCH
+const wordList: string = ["bauble", "christmas", "cracker", "elf", "present", 
+"santa", "reindeer", "snow", "tinsel", "carols"];
+
+// FIGURING OUT HOW MANY RANDOM LETTERS I NEED
+const totalListCharacters: number = wordList.join("").length;
+console.log(totalListCharacters)
+
+// GENERATING THE RANDOM LETTERS
+const characters = "abcdefghijklmnopqrstuvwxyz";
+
+const generateRandomString = (length:number) => {
+  let result = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++){
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+console.log(generateRandomString(83));
+
 
