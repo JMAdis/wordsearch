46b6b8beba98a72b917efbb4cf24b1965a2f9247
J M Adis <jessicaadis7@gmail.com>
2023-12-05 14:46:23 +0000

    got random letters to display!

--- wordsearch/src/main.ts
+++ wordsearch/src/main.ts
@@ -1,7 +1,7 @@
 import "../styles/main.scss"
 
 // QUERY SELECTOR FOR WORDSEARCH GRID
-const wordsearchGrid = document.querySelector<HTMLElement>("#wordsearch-grid")
+const wordsearchGrid = document.querySelector<HTMLElement>("#wordsearch-grid") ?? document.createElement("div")
 
 // ELEMENT VALIDATION
 if(!wordsearchGrid){
@@ -9,11 +9,11 @@ if(!wordsearchGrid){
 }
 
 // DEFININING WORD LIST FOR WORDSEARCH
-const wordsToFindList: string[] = ["BAUBLE", "CHRISTMAS", "CRACKER", "ELF", "PRESENT", 
+const listOfWordsToFind: string[] = ["BAUBLE", "CHRISTMAS", "CRACKER", "ELF", "PRESENT", 
 "SANTA", "REINDEER", "SNOW", "TINSEL", "CAROLS"];
 
 // FIGURING OUT HOW MANY RANDOM LETTERS I NEED
-const totalListCharacters: number = wordsToFindList.join("").length;
+const totalListCharacters: number = listOfWordsToFind.join("").length;
 console.log(totalListCharacters)
 
 // GENERATING THE RANDOM LETTERS
@@ -30,5 +30,30 @@ const generateRandomString = (length:number) => {
 
 console.log(generateRandomString(83));
 
-// 
+// DIMENSIONS OF THE WORDSEARCH
+const rows = 12
+const columns = 12
 
+// SETTING THE ROWS AND COLUMNS FOR THE GRID
+function initializeWordsearch() {
+  wordsearchGrid.style.setProperty("__rows", `${rows}`);
+  wordsearchGrid.style.setProperty("__columns", `${columns}`);
+
+  // FOR LOOP TO CREATE A NEW DIV AND SETS THE TEXT CONTENT
+  for (let i = 0; i < rows * columns; i++){
+    const cell = document.createElement("div");
+    cell.innerText = generateRandomString(1);
+    wordsearchGrid.appendChild(cell)
+  }
+}
+
+console.log(initializeWordsearch())
+
+
+/* ADDING THE WORDS TO FIND ARRAY TO THE WORDSEARCH GRID
+let word = 1
+let individualWords = listOfWordsToFind.slice(0, word);
+console.log(individualWords)
+
+wordsearchGrid.innerHTML = generateRandomString(1);
+*/
