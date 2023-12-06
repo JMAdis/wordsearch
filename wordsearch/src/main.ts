9aa601c9a1a0d7a5df7e3779db4ea6558fa40c10
J M Adis <jessicaadis7@gmail.com>
2023-12-06 15:58:54 +0000

    may have found a solution - committing code so far

--- wordsearch/src/main.ts
+++ wordsearch/src/main.ts
@@ -12,6 +12,11 @@ if(!wordsearchGrid){
 const listOfWordsToFind: string[] = ["BAUBLE", "CHRISTMAS", "CRACKER", "ELF", "PRESENT", 
 "SANTA", "REINDEER", "SNOW", "TINSEL", "CAROLS"];
 
+//BREAKING THE ARRAY INTO NESTED ARRAYS
+const listOfLetters: string[][] = listOfWordsToFind.map(word => word.split(""));
+
+console.log(listOfLetters[1])
+
 // FIGURING OUT HOW MANY RANDOM LETTERS I NEED
 const totalListCharacters: number = listOfWordsToFind.join("").length;
 console.log(totalListCharacters)
@@ -34,26 +39,90 @@ console.log(generateRandomString(83));
 const rows = 12
 const columns = 12
 
+const horizontal = Math.random() < 0.8;
+
 // SETTING THE ROWS AND COLUMNS FOR THE GRID
+function isInBounds(startX, startY, word){
+  return startX + word.length <= columns && startY + word.length <= rows;
+}
+
 function initializeWordsearch() {
-  wordsearchGrid.style.setProperty("__rows", `${rows}`);
+  for (let i = 0; i < listOfWordsToFind.length; i++) {
+    let startX, startY;
+    
+    do{
+      startX = Math.floor(Math.random() * columns);
+      startY = Math.floor(Math.random() * rows);
+    } while (!isInBounds(startX, startY, listOfLetters[i]))
+
+    placeWord(startX, startY, listOfLetters[i])
+
+    for (let j = 0; j < 5; j++){
+      fillRandomCell();
+    }
+  }
+
+  while(moreCellsToFill()){
+    fillRandomCell();
+  }
+}
+
+function placeWord(x, y, word){
+  word.forEach((letter, index) => {
+
+    const cell = document.createElement("div");
+    cell.innerText = letter;
+    cell.style.top = `${y + index}px`;
+    cell.style.left = `${x + index}px`;
+    wordsearchGrid.appendChild(cell);
+  });
+};
+
+function fillRandomCell(){
+}
+
+initializeWordsearch()
+
+/*  wordsearchGrid.style.setProperty("__rows", `${rows}`);
   wordsearchGrid.style.setProperty("__columns", `${columns}`);
+  wordsearchGrid.innerHTML = "";
+
+  listOfLetters.forEach((word, wordIndex) => {
+    const startX = Math.floor(Math.random() * columns - word.length + 1);
+    const startY = Math.floor(Math.random() * rows - word.length + 1);
+
+    
+
+    word.forEach((letter, letterIndex) =>{
+      const cell = document.createElement("div");
+      const x = horizontal ? startX + letterIndex : startX;
+      const y = horizontal ? startY : startY + letterIndex;
+
+      if (x < columns && y < rows) {
+        cell.innerText = letter;
+        wordsearchGrid.appendChild(cell);
+      }
+    });
+  });
 
   // FOR LOOP TO CREATE A NEW DIV AND SETS THE TEXT CONTENT
-  for (let i = 0; i < rows * columns; i++){
+  for (let i = listOfWordsToFind.join("").length; i < rows * columns; i++){
     const cell = document.createElement("div");
     cell.innerText = generateRandomString(1);
     wordsearchGrid.appendChild(cell)
   }
 }
 
-console.log(initializeWordsearch())
+initializeWordsearch()
+
+*/
 
 
-/* ADDING THE WORDS TO FIND ARRAY TO THE WORDSEARCH GRID
+
+/* ADDING THE WORDSTOFIND ARRAY TO THE WORDSEARCH GRID
 let word = 1
 let individualWords = listOfWordsToFind.slice(0, word);
 console.log(individualWords)
 
-wordsearchGrid.innerHTML = generateRandomString(1);
+wordsearchGrid.innerHTML = 
 */
